0e33db7fdb6e5703044a4db6bfc8e4058431e2ad
Tinkle22 <mutalemattlesa@gmail.com>
2026-02-04 18:55:43 +0200

    conne

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,17 +13,17 @@ export default defineConfig({
     https: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5001',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'http://127.0.0.1:5001',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
       '/socket.io': {
-        target: 'http://127.0.0.1:5001',
+        target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
         ws: true,
